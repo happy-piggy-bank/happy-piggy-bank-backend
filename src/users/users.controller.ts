ffdaaf882e111a3733837c0bc43271119bf6bc84
Gary Lee <dokdo2005@gmail.com>
2021-12-31 16:24:51 +0900

    유저 route 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UsersService } from './users.service';
@@ -7,9 +7,24 @@ import { UsersService } from './users.service';
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
+    @Post('login')
+    async loginUser() {}
+
     @Post('join')
     async createUser(@Body() userData: CreateUserDto, @Res() res: Response) {
         const result = await this.usersService.createUser(userData);
         return res.status(result.statusCode).send(result);
     }
+
+    @Post('logout')
+    async lougoutUser() {}
+
+    @Get('myInfo')
+    async getUserInfo() {}
+
+    @Put('myInfo')
+    async modifyUserInfo() {}
+
+    @Delete('leave')
+    async deteleUser() {}
 }
