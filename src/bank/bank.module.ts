7c12e89b814b32b5b81cd240574e3e006657f4a3
Gary Lee <dokdo2005@gmail.com>
2021-12-31 15:59:07 +0900

    저금통 module에 TypeORM import 추가

--- src/bank/bank.module.ts
+++ src/bank/bank.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PiggyBank } from 'src/entities/piggyBank.entity';
 import { BankController } from './bank.controller';
 import { BankService } from './bank.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([PiggyBank])],
   controllers: [BankController],
   providers: [BankService],
   exports: [BankService]
