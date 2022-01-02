63ca5ffcae4f93b5bccc015c1dfb4b593bea6339
Gary Lee <dokdo2005@gmail.com>
2022-01-02 23:46:24 +0900

    middleware 수정

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -10,23 +10,23 @@ export class AppMiddleware implements NestMiddleware {
 
   async use(req: Request | any, res: Response, next: NextFunction) {
     if (!req.headers.token) {
-      return {
+      res.status(HttpStatus.BAD_REQUEST).send({
         statusCode: HttpStatus.BAD_REQUEST,
         result: "no_auth_token",
         message: "인증 토큰 없음"
+      });
+    } else {
+      const checkToken = await this.jwtService.verifyJwtToken(req.headers.token);
+      if (checkToken === 'INVALID') {
+        res.status(HttpStatus.UNAUTHORIZED).send({
+          statusCode: HttpStatus.UNAUTHORIZED,
+          result: "invalid_token",
+          message: "유효하지 않은 인증 토큰"
+        });
+      } else {
+        res.locals.userId = checkToken;
+        next();
       }
     }
-
-    const checkToken = await this.jwtService.verifyJwtToken(req.headers.token);
-    if (checkToken === 'INVALID') {
-      return {
-        statusCode: HttpStatus.UNAUTHORIZED,
-        result: "invalid_token",
-        message: "유효하지 않은 인증 토큰"
-      }
-    }
-    
-    res.locals.userId = checkToken;
-    next();
   }
 }
