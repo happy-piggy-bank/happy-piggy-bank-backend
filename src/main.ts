a1f2854c08f984a3b2c1e34c163b94078af0742c
Gary Lee <dokdo2005@gmail.com>
2022-01-02 23:52:40 +0900

    CSRF 공격 방지

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(csurf());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
