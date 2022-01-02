b96022a1445214119551a7c9b044329cfb6c38e0
Gary Lee <dokdo2005@gmail.com>
2022-01-02 21:50:36 +0900

    JWT 공용 서비스에서 불필요한 import 삭제

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,4 +1,4 @@
-import { HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
