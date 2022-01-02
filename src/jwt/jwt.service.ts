5a795f3add1d7f67ec45f315c774f91a81caa142
Gary Lee <dokdo2005@gmail.com>
2022-01-02 23:46:36 +0900

    jwt 공용 서비스 로직 수정

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -13,24 +13,25 @@ export class JwtService {
 
     async getJwtToken(userId: number) {
         const token = jwt.sign({ userId }, process.env.JWT_SECRET);
-        await this.users.update({ id: userId }, { authToken: token })
+        await this.users.update({ id: userId }, { authToken: token });
         return token;
     }
 
     async verifyJwtToken(token: string) {
-        let result = '';
-        jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-            if (err || !decoded.userId) {
-                result = 'INVALID';
+        try {
+            const verifyResult = jwt.verify(token, process.env.JWT_SECRET);
+            if (!verifyResult['userId']) {
+                return 'INVALID';
             } else {
-                const checkResult = await this.users.count({ id: decoded.userId, authToken: token })
-                if (checkResult <= 0) {
-                    result = 'INVALID';
+                const checkResult = await this.users.count({ authToken: token });
+                if (checkResult > 0) {
+                    return verifyResult['userId'];
                 } else {
-                    result = decoded.userId;
+                    return 'INVALID';
                 }
             }
-        })
-        return result;
+        } catch (err) {
+            return 'INVALID';
+        }
     }
 }
