4cf6758a72c8c68a3b9f0e1ad4c7bf7bfb45a0d5
Gary Lee <dokdo2005@gmail.com>
2022-01-10 04:26:10 +0900

    Revert "회원정보 업데이트 시 빈칸 처리"
    This reverts commit 7024cb96f5a11301a290bbb43c69a1c2fd47adf6.

--- src/users/dtos/updateUser.dto.ts
+++ src/users/dtos/updateUser.dto.ts
@@ -1,14 +1,11 @@
-import { Transform } from "class-transformer";
 import { IsOptional, IsString } from "class-validator";
 
 export class UpdateUserDto {
     @IsOptional()
     @IsString()
-    @Transform(({ value }) => value.trim())
     readonly userPw: string;
 
     @IsOptional()
     @IsString()
-    @Transform(({ value }) => value.trim())
     readonly userName: string;
 }
